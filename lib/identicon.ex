9cf678c4c3002104334edb80776ac26fbc14615c
Alexey Faizullov <alex.fuzza@gmail.com>
2018-06-10 22:31:22 +0300

    WIP grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,4 +2,38 @@ defmodule Identicon do
   @moduledoc """
   Documentation for Identicon.
   """
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: [r, g, b]}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
 end
